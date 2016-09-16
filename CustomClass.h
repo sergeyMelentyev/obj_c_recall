94c328eff9be3438d77f5e29699e559c3493fc64
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-09-16 18:38:57 +0400

    Update CustomClass.h

--- CustomClass.h
+++ CustomClass.h
@@ -1,9 +1,18 @@
 #import <Foundation/Foundation.h>
 
 
-/* USE INSTANCE VARIABLE */
-@interface CustomClass : NSObject 
+/* PRIVATE CLASS EXTENSION (NO SUBCLASS ACCESS) */
+@interface CustomClass ()
+{
+	// instance variables
+	NSMutableArray *_assets;
+}
+	@property (nonatomic) unsigned int anyName;
+@end
 
+
+/* GLOBAL CLASS INTERFACE USING INSTANCE VARIABLE */
+@interface CustomClass : NSObject 
 {
 	// instance variables
 	float _heightInMeters;
@@ -17,18 +26,20 @@
 	- (void) setWeightInKilos:(int)weight;
 
 	- (float) bodyMassIndex;
-
 @end
 
 
-/* USE PROPERTIES */
-@interface CustomClass : NSObject 
-
+/* GLOBAL CLASS INTERFACE USING PROPERTIES */
+@interface CustomClass : NSObject <NSURLConnectionDelegate, NSURLConnectionDataDelegate>
+{
+	// instance variables
+	NSMutableArray *_assets;
+	NSMutableData *_incomingData;
+}
 	// properties
 	@property (nonatomic) float heightInMeters;	// compiler will create _heightInMeters
 	@property (nonatomic, readonly) int weightInKilos;
 
 	// instance methods
 	- (float) bodyMassIndex;
-
 @end
